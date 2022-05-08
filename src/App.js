182ed64bf80caa3499332cb8b5ec10cba9447fa5
asiwajudev <olanrewajuranda@gmail.com>
2022-05-08 15:23:13 +0100

    Added lint config file

--- src/App.js
+++ src/App.js
@@ -1,32 +1,33 @@
+import React from "react";
 import "./App.css";
 
 function App() {
   return (
-    <section class="bg-gray-50">
-      <div class="max-w-screen-xl px-4 py-32 mx-auto lg:h-screen lg:items-center lg:flex">
-        <div class="max-w-xl mx-auto text-center">
-          <h1 class="text-3xl font-extrabold sm:text-5xl">
+    <section className="bg-gray-50">
+      <div className="max-w-screen-xl px-4 py-32 mx-auto lg:h-screen lg:items-center lg:flex">
+        <div className="max-w-xl mx-auto text-center">
+          <h1 className="text-3xl font-extrabold sm:text-5xl">
             Understand User Flow.
-            <strong class="font-extrabold text-red-700 sm:block">
+            <strong className="font-extrabold text-red-700 sm:block">
               Increase Conversion.
             </strong>
           </h1>
 
-          <p class="mt-4 sm:leading-relaxed sm:text-xl">
+          <p className="mt-4 sm:leading-relaxed sm:text-xl">
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt
             illo tenetur fuga ducimus numquam ea!
           </p>
 
-          <div class="flex flex-wrap justify-center gap-4 mt-8">
+          <div className="flex flex-wrap justify-center gap-4 mt-8">
             <a
-              class="block w-full px-12 py-3 text-sm font-medium text-white bg-red-600 rounded shadow sm:w-auto active:bg-red-500 hover:bg-red-700 focus:outline-none focus:ring"
+              className="block w-full px-12 py-3 text-sm font-medium text-white bg-red-600 rounded shadow sm:w-auto active:bg-red-500 hover:bg-red-700 focus:outline-none focus:ring"
               href="/get-started"
             >
               Get Started
             </a>
 
             <a
-              class="block w-full px-12 py-3 text-sm font-medium text-red-600 rounded shadow sm:w-auto hover:text-red-700 active:text-red-500 focus:outline-none focus:ring"
+              className="block w-full px-12 py-3 text-sm font-medium text-red-600 rounded shadow sm:w-auto hover:text-red-700 active:text-red-500 focus:outline-none focus:ring"
               href="/about"
             >
               Learn More
