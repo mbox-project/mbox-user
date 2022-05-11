ac17e956ee550d2931e0ffb35d0d4a8a870b0313
Prudence2020 <uzoekwelevisonc@gmail.com>
2022-05-10 19:12:50 -0700

    updating my codes

--- src/pages/ForgotPassword/ForgotPassword.jsx
+++ src/pages/ForgotPassword/ForgotPassword.jsx
@@ -1,5 +1,3 @@
-import React from "react";
-const  ForgotPassword =() => {
-
-}
-export default ForgotPassword;
+// import React from "react";
+const  ForgotPassword = () => {};
+export default ForgotPassword ;
