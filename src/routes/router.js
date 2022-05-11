4778853e84372b1d5570c0a648c03bfe393055e3
Prudence2020 <uzoekwelevisonc@gmail.com>
2022-05-11 07:29:28 -0700

    Updating the ForgotPassword page

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import { LandingPage } from "../pages/Landing";
+import { ForgotPassword } from "../pages/ForgotPassword";
 
 const RouteController = () => {
   return (
     <Routes>
-      <Route path="/" element={<LandingPage />} exact />
+      <Route path="/" element={<LandingPage />}  />
+      <Route path="/forgot-password" element={<ForgotPassword />} exact  />
     </Routes>
   );
 };
