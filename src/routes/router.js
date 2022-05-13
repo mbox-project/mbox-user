972e47f4a0648021ac8f2dd558dcba51e3838976
asiwajudev <olanrewajuranda@gmail.com>
2022-05-13 21:04:44 +0100

    Added Page Layout

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
+import { Register } from "../pages/Auth";
 import { LandingPage } from "../pages/Landing";
 
 const RouteController = () => {
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} exact />
+      <Route path="/login" element={<Register />} />
     </Routes>
   );
 };
