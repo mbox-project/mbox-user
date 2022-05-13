d0ffa165473544d1030cb54c2ddd7a757afffe2f
asiwajudev <olanrewajuranda@gmail.com>
2022-05-13 21:04:44 +0100

    Added Page Layout

---
+++ src/pages/Auth/Register/Register.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import logo from "../../../assets/images/logo.png";
+function Register() {
+  return (
+    <div className="w-full h-screen flex">
+      <div className="w-1/2 mx-auto p-12 bg-black">
+        <img src={logo} />
+      </div>
+      <div className="w-1/2 mx-auto bg-white p-12">
+        <div className="w-full flex justify-center">
+          <h1 className="text-5xl text-orange-600">Login</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Register;
--- src/routes/router.js
+++ src/routes/router.js
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
+import { Register } from "../pages/Auth";
 import { LandingPage } from "../pages/Landing";
 import { ForgotPassword } from "../pages/ForgotPassword";
 
 const RouteController = () => {
   return (
     <Routes>
-      <Route path="/" element={<LandingPage />} />
-      <Route path="/forgot-password" element={<ForgotPassword />} exact />
+      <Route path="/" element={<LandingPage />} exact />
+      <Route path="/forgot-password" element={<ForgotPassword />} />
+      <Route path="/login" element={<Register />} />
     </Routes>
   );
 };
