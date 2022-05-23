d242991a4a554b2a17859b11657f5f7b369ece4d
Prudence2020 <uzoekwelevisonc@gmail.com>
2022-05-22 17:58:35 -0700

    Made the modal responsive

--- src/pages/Modal/Modal.jsx
+++ src/pages/Modal/Modal.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import email from "../../assets/images/email.png";
 const Modal = () => {
   return (
-    <div className="w-full p-4 lg:w-1/2 m-auto border-2 mt-0 lg:mt-20  font-poppins ">
-      <img src={email} className="" />
-      <h5 className="flex  justify-center font-extrabold text-base">
+    <div className="  p-4  m-4 rounded lg:w-1/2 lg:m-auto  mt-12 lg:mt-20 shadow font-poppins ">
+      <img src={email} className=" flex flex-col justify-center items-center" />
+      <h5 className="flex text-lg md:text-2xl justify-center font-extrabold lg:text-base">
         Email Confirmation
       </h5>
-      <p className="text-base">
+      <p className=" leading-20  text-lg md:text-xl lg:text-base pt-5 text-center">
         We have sent an Email to Taylormason@gmail.com to continue this process,
         After receiving the Email follow the link provided to complete your
         registration{" "}
