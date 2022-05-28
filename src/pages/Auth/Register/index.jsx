d7a7e3e6851a1ecb020337888c93df54e9f122eb
Prudence2020 <uzoekwelevisonc@gmail.com>
2022-05-27 20:35:57 -0700

    made some changes in register page

--- src/pages/Auth/Register/index.jsx
+++ src/pages/Auth/Register/index.jsx
@@ -21,17 +21,17 @@ function Register() {
           </p>
         </div>
       </div>
-      <div className="w-full lg:w-1/2  lg:mx-auto bg-white ">
+      <div className="w-full lg:w-1/2  lg:mx-auto generalbg ">
         <div className="w-full  font-poppins text-center mt-12 ">
           <h1 className="lg:text-5xl text-orange-600 text-4xl md:text-5xl  mt-20 lg:mt-24 lg:pt-0 font-poppins font-extrabold ">
             Create Account
           </h1>
-          <p className=" hidden lg:flex text-sm pt-3 justify-center px-48 leading-relaxed ">
+          <p className=" flex  text-base max-w-xs md:max-w-none lg:text-sm mt-4 lg:mt-3 pl-16 md:pl-0 justify-center lg:px-48 leading-relaxed ">
             Lets get you all set up. Provide us with the following information
             to get started
           </p>
         </div>
-        <form className="px-10 mt-4 lg:pt-0">
+        <form className=" px-5 lg:px-10 mt-5 lg:mt-4 lg:pt-0 ">
           <div className="mb-4">
             <Label
               className="w-full pb-1 text-base text-[#9A9A9A]"
@@ -41,7 +41,7 @@ function Register() {
             <Input
               name="text"
               type="text"
-              className="lg:w-full  w-full mt-1 p-4  lg:p-2 text-lg lg:text-base border-[#444444] rounded-md shadow-sm border-2 "
+              className=" w-full mt-1 p-2 inputcolor focus:outline-none lg:p-2 text-lg lg:text-base border-[#444444] rounded-md shadow-sm border-2 "
               required={true}
               autoFocus={false}
             />
@@ -56,7 +56,7 @@ function Register() {
             <div className="relative">
               <input
                 type="email"
-                className="w-full p-4  lg:p-2  pr-12 mt-1  text-lg lg:text-base border-[#444444] border-2 rounded-md shadow-sm"
+                className="w-full p-2  lg:p-2  pr-12 mt-1 inputcolor  focus:outline-none  text-lg lg:text-base border-[#444444] border-2 rounded-md shadow-sm"
               />
             </div>
           </div>
@@ -70,7 +70,7 @@ function Register() {
             <div className="relative">
               <input
                 type="number"
-                className="w-full p-4  lg:p-2  pr-12 mt-1  text-lg lg:text-base border-[#444444] border-2 rounded-md shadow-sm"
+                className="w-full p-2  lg:p-2  pr-12 mt-1 inputcolor focus:outline-none  text-lg lg:text-base border-[#444444] border-2 rounded-md shadow-sm"
               />
             </div>
           </div>
@@ -84,7 +84,7 @@ function Register() {
             <div className="relative">
               <input
                 type="text"
-                className="w-full p-4  lg:p-2  pr-12 mt-1  text-lg lg:text-base border-[#444444] border-2 rounded-md shadow-sm"
+                className="w-full p-2  lg:p-2  pr-12 mt-1 inputcolor focus:outline-none text-lg lg:text-base border-[#444444] border-2 rounded-md shadow-sm"
               />
             </div>
           </div>
@@ -98,7 +98,7 @@ function Register() {
             <div className="relative">
               <input
                 type="password"
-                className="w-full p-4  lg:p-2  pr-12 mt-1  text-lg lg:text-base border-[#444444] border-2 rounded-md shadow-sm"
+                className="w-full p-2  lg:p-2  pr-12 mt-1 inputcolor focus:outline-none text-lg lg:text-base border-[#444444] border-2 rounded-md shadow-sm"
               />
               <p className=" text-sm text-orange-600 pt-2">
                 Password should contain at least 8 characters
@@ -115,27 +115,27 @@ function Register() {
             <div className="relative">
               <input
                 type="password"
-                className="w-full p-4  lg:p-2  pr-12 mt-1 text-lg lg:text-base border-[#444444] border-2 rounded-md shadow-sm"
+                className="w-full p-2  lg:p-2  pr-12 mt-1 inputcolor focus:outline-none text-lg lg:text-base border-[#444444] border-2 rounded-md shadow-sm"
               />
             </div>
           </div>
 
-          <Button className="w-full bg-orange-600 p-4  lg:p-2  mb-4 rounded-md text-white text-2xl font-semibold">
+          <Button className="w-full bg-orange-600 p-2 mt-3 lg:p-2  shadow-lg rounded-md text-white text-lg font-semibold">
             Sign Up
           </Button>
           <div className="flex justify-center mb-3">
-            <span className="text-gray-600 text-sm pt-2">
+            <span className="text-gray-600 text-sm mt-2 lg:mt-1">
               By Sign Up, you’ve already agreed to our{" "}
               <Link to="/" className="text-gray-900 font-bold">
                 Terms & Condition
               </Link>
             </span>
           </div>
-          <div className="flex">
-            <p className="mr-3 text-gray-600 text-xl">Existing User?</p>
+          <div className="flex ">
+            <p className="mr-3 mt-3 text-gray-600 text-xl">Existing User?</p>
             <Link
               to="/auth/login"
-              className="text-orange-600 text-xl underline"
+              className="text-orange-600 mt-3 text-xl underline"
             >
               Login
             </Link>
