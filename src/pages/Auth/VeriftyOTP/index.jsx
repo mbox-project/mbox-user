9e1d5122de8663a20dca168923beb3a37478825c
Prudence2020 <uzoekwelevisonc@gmail.com>
2022-06-01 06:26:51 -0700

    commit message

---
+++ src/pages/Auth/VeriftyOTP/index.jsx
@@ -0,0 +1,125 @@
+import React from "react";
+import icon from "../../../assets/images/icon.png";
+import logo from "../../../assets/images/logo.png";
+import { Link } from "react-router-dom";
+import Button from "../../../components/Button";
+import Input from "../../../components/Input";
+import Footer from "../../../components/Footer";
+function VerifyOTP() {
+  return (
+    <div className="flexay ">
+      <div className=" hidden  min-h-screen w-1/2  mx-auto  lg:block bg-verify-img bg-cover ">
+        <img src={logo} className=" w-32 m-6" />
+        <div className=" mt-96 pt-20 text-white">
+          <img src={icon} className=" w-32  mx-auto " />
+          <p className="flex justify-center  text-2xl font-bold font-poppins -mt-20">
+            Hello, Friend!{" "}
+          </p>
+          <p className="flex justify-center text-lg ">
+            Welcome back! Enter the OTP sent to your email to continue to enjoy
+            MBOX
+          </p>
+        </div>
+      </div>
+      <div className="w-full lg:w-1/2  lg:mx-auto generalbg bg-cover ">
+        <div className="w-full  font-poppins text-center mt-12 ">
+          <h1 className="lg:text-3xl text-orange-600 text-4xl  mt-32 lg:mt-44 lg:pt-0 font-poppins font-extrabold ">
+            OTP Verification
+          </h1>
+          <p className="block pb-3 pt-4 text-base lg:text-xs md:pt-6  lg:pt-3 px-12 lg:px-52 leading-relaxed ">
+            Enter the 6-digit number sent to taylormason@gmail.com
+          </p>
+        </div>
+        <form className="px-10 pt-4  md:mt-4 lg:mt-6">
+          <div className="mb-4 flex justify-center ">
+            <div className=" mx-0 md:mx-4 lg:mx-6 ">
+              <div className="relative">
+                <Input
+                  name="number"
+                  type="number"
+                  className=" w-2 md:w-4 py-2 px-6 inputcolor mr-2 mt-2 text-base  border-[#444444] border-2 rounded-md shadow-sm"
+                  required={true}
+                  autoFocus={false}
+                />
+              </div>
+            </div>
+            <div className="mx-0 md:mx-4 lg:mx-6">
+              <div className="relative">
+                <Input
+                  name="number"
+                  type="number"
+                  className=" w-2 md:w-4 py-2 px-6 inputcolor mr-2 mt-2 text-base  border-[#444444] border-2 rounded-md shadow-sm"
+                  required={true}
+                  autoFocus={false}
+                />
+              </div>
+            </div>
+            <div className="mx-0 md:mx-4 lg:mx-6">
+              <div className="relative">
+                <Input
+                  name="number"
+                  type="number"
+                  className=" w-2 md:w-4 py-2 px-6 inputcolor mr-2 mt-2 text-base  border-[#444444] border-2 rounded-md shadow-sm"
+                  required={true}
+                  autoFocus={false}
+                />
+              </div>
+            </div>
+            <div className="mx-0 md:mx-4 lg:mx-6">
+              <div className="relative">
+                <Input
+                  name="number"
+                  type="number"
+                  className=" w-2 md:w-4 py-2 px-6 inputcolor mr-2 mt-2 text-base  border-[#444444] border-2 rounded-md shadow-sm"
+                  required={true}
+                  autoFocus={false}
+                />
+              </div>
+            </div>
+            <div className="mx-0 md:mx-4 lg:mx-6">
+              <div className="relative">
+                <Input
+                  name="number"
+                  type="number"
+                  className=" w-2 md:w-4 py-2 px-6 inputcolor mr-2 mt-2 text-base  border-[#444444] border-2 rounded-md shadow-sm"
+                  required={true}
+                  autoFocus={false}
+                />
+              </div>
+            </div>
+            <div className="mx-0 md:mx-4 lg:mx-6">
+              <div className="relative">
+                <Input
+                  name="number"
+                  type="number"
+                  className=" w-2 md:w-4 py-2 px-6 inputcolor mr-2 mt-2 text-base  border-[#444444] border-2 rounded-md shadow-sm"
+                  required={true}
+                  autoFocus={false}
+                />
+              </div>
+            </div>
+          </div>
+          <div className="flex justify-center mb-3">
+            <Link to="/" className="text-gray-900  underline font-bold pl-2">
+              Request new code
+            </Link>
+          </div>
+
+          <Button className="w-full bg-orange-600 p-2 mb-4 mt-10 md:mt-12 lg:mt-7 rounded-md shadow-md text-white text-lg font-semibold">
+            Verify and Proceed
+          </Button>
+        </form>
+
+        <Footer>
+          <div className="flex font-poppins  md:text-2xl md:mt-44  font-light justify-center pt-60 text-base lg:text-xs  lg:mt-32">
+            <p className="font-bold text-3xl pt-2 md:pt-3 lg:pt-2 ">&copy;</p>
+            <p className="px-1 pt-4"> 2021</p>
+            <p className="pt-4"> All copyright Reserved</p>
+          </div>
+        </Footer>
+      </div>
+    </div>
+  );
+}
+
+export default VerifyOTP;
--- src/pages/Modal/Modal.jsx
+++ src/pages/Modal/Modal.jsx
@@ -1,17 +1,43 @@
 import React from "react";
 import email from "../../assets/images/email.png";
+import { Link } from "react-router-dom";
 const Modal = () => {
   return (
-    <div className="  p-4  m-4 rounded lg:w-1/2 lg:m-auto  mt-12 lg:mt-20 shadow font-poppins ">
-      <img src={email} className=" flex flex-col justify-center items-center" />
+    <div className="  p-4  m-4 rounded lg:w-1/2 lg:m-auto  mt-12 lg:mt-20 shadow font-poppins  ">
+      <div className="flex ">
+        <img src={email} className="w-10 h-15 " />
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="w-5 h-5 "
+          viewBox="0 0 20 20"
+          fill="currentColor"
+        >
+          <path d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" />
+        </svg>
+      </div>
+
       <h5 className="flex text-lg md:text-2xl justify-center font-extrabold lg:text-base">
         Email Confirmation
       </h5>
-      <p className=" leading-20  text-lg md:text-xl lg:text-base pt-5 text-center">
+      <p className="leading-relaxed  text-lg md:text-xl lg:text-base pt-5 text-center">
         We have sent an Email to Taylormason@gmail.com to continue this process,
         After receiving the Email follow the link provided to complete your
         registration{" "}
       </p>
+
+      <hr className=" mt-6" />
+
+      <div className="  pt-4 md:pt-0 block md:flex justify-center ">
+        <p className=" mr-0 mt-0 md:mr-3 md:mt-3 text-gray-600   text-center text-lg md:text-xl lg:text-base">
+          I didnt get any e-mail?
+        </p>
+        <Link
+          to="/auth/login"
+          className="text-orange-600 mt-3 flex justify-center  text-lg md:text-xl lg:text-base "
+        >
+          Resend Confirmation Email
+        </Link>
+      </div>
     </div>
   );
 };
