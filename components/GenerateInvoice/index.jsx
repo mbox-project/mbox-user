4faa44d57cb6d14d3e47d73a0b273df8fc8661e1
Prudence2020 <uzoekwelevisonc@gmail.com>
2022-06-12 15:34:41 -0700

    added the generate invoice component

--- components/GenerateInvoice/index.jsx
+++ components/GenerateInvoice/index.jsx
@@ -1,50 +1,53 @@
 import React from "react";
+import Button from "../Button";
+import Image from "next/dist/client/image";
+import picture from "../../public/images/generateinvoiceimg.png";
+import pics from "../../public/images/generateinvoiceimg2.png";
 const GenerateInvoice = () => {
   return (
-    <section>
+    <section className="featurebg">
       <div className="max-w-screen-xl px-4 py-16 mx-auto sm:px-6 lg:px-8">
-        <div className="lg:flex lg:gap-8">
-          <div>
-            <span className="block w-32 h-10 bg-gray-700 rounded-lg"></span>
+        <div className="grid grid-cols-2 gap-20 mt-8 lg:mt-0 lg:grid-cols-5 lg:gap-y-16">
+          <div className="col-span-2">
+            <div>
+              <h2 className="text-2xl font-bold ">
+                Generate/Pay <span className="text-orange-600">Invoice</span>
+              </h2>
+
+              <p className="mt-6 text-gray-400">
+                Prepare an invoice for their customers to transaction via an
+                ESCROW. For optimum financial security.
+              </p>
+
+              <Button className="w-full h-12 px-6 py-3  uppercase lg:normal-case lg:rounded-md  lg:mt-12 text-sm font-bold tracking-wide  text-orange-600 sm:ml-4 sm:flex-shrink-0 sm:w-auto mt-3 bg-white border-2 border-orange-600  lg:ml-0">
+                Generate/Pay Invoice
+              </Button>
+            </div>
           </div>
 
-          <div className="grid grid-cols-2 gap-8 mt-8 lg:mt-0 lg:grid-cols-5 lg:gap-y-16">
-            <div className="col-span-2">
-              <div>
-                <h2 className="text-2xl font-bold text-white">
-                  Get the latest news!
-                </h2>
+          <div className="col-span-2 lg:col-span-3 lg:flex lg:ml-96">
+            <Image src={picture}
+              />
+          </div>
+        </div>
+        <div className="grid grid-cols-2 gap-20 mt-8 lg:mt-0 lg:grid-cols-5 lg:gap-y-16">
+          <div className="col-span-2 lg:col-span-3 lg:flex">
+            <Image src={pics} className="ml-20 w-8" />
+          </div>
+          <div className="col-span-2">
+            <div>
+              <h2 className="text-2xl font-bold text-orange-600 ">
+                Endorse <span className="text-black">Businesses</span>
+              </h2>
 
-                <p className="mt-6 text-gray-400">
-                  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Esse
-                  non cupiditate quae nam molestias.
-                </p>
-              </div>
-            </div>
+              <p className="mt-6 text-gray-400">
+                Express your support for friends business by endorsing their
+                business on Mbox. We rise by lifting others.
+              </p>
 
-            <div className="col-span-2 lg:col-span-3 lg:flex lg:items-end">
-              <form className="w-full text-white">
-                <label htmlFor="email" className="sr-only">
-                  {" "}
-                  Email{" "}
-                </label>
-
-                <div className="p-2 border sm:flex sm:items-center border-white/10">
-                  <input
-                    className="w-full h-12 p-3 text-sm font-medium tracking-widest placeholder-gray-400 uppercase bg-transparent border-none"
-                    type="email"
-                    id="email"
-                    placeholder="Enter your email"
-                  />
-
-                  <button
-                    className="w-full h-12 px-6 py-3 mt-1 text-sm font-bold tracking-wide uppercase bg-red-700 sm:ml-4 sm:flex-shrink-0 sm:w-auto sm:mt-0"
-                    type="submit"
-                  >
-                    Sign Up
-                  </button>
-                </div>
-              </form>
+              <Button className="w-full h-12 px-6 py-3  uppercase lg:normal-case lg:rounded-md  lg:mt-12 text-sm font-bold tracking-wide  text-orange-600 sm:ml-4 sm:flex-shrink-0 sm:w-auto mt-3 bg-white border-2 border-orange-600  lg:ml-0">
+                Endorse a business
+              </Button>
             </div>
           </div>
         </div>
