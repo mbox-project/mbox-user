e7a7055ef3712281e4d6558e09f090b93dd6974c
asiwaju <olanrewajuranda@gmail.com>
2022-06-22 21:52:23 +0100

    Fixed errors on the branch

--- components/BuyerNav/MobileSidebar.jsx
+++ components/BuyerNav/MobileSidebar.jsx
@@ -1,81 +1,78 @@
-import React from 'react'
-import Image from "next/image"
-import Link from "next/link"
-import { BsCartDash } from "react-icons/bs"
-import profile from "../../public/buyersImg/profile.svg"
-import purchase from "../../public/buyersImg/purchase.svg"
-import report from "../../public/buyersImg/report.svg"
-import saved from "../../public/buyersImg/saved.svg"
-import wallet from "../../public/buyersImg/wallet.svg"
-import thumb from "../../public/buyersImg/thumb.svg"
-
-
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { BsCartDash } from "react-icons/bs";
+import profile from "../../public/buyersImg/profile.svg";
+import purchase from "../../public/buyersImg/purchase.svg";
+import report from "../../public/buyersImg/report.svg";
+import saved from "../../public/buyersImg/saved.svg";
+import wallet from "../../public/buyersImg/wallet.svg";
+import thumb from "../../public/buyersImg/thumb.svg";
 
 const MobileSidebar = ({ showSideBar }) => {
+  return (
+    <>
+      {/* Mobile Menu sidebar */}
+      {showSideBar && (
+        <aside className="w-1/2 bg-white z-30 flex flex-col absolute top-0 left-0 shadow-md h-full md:hidden">
+          <h6 className="py-4 font-bold text-lg pl-10 text-white bg-brightRed">
+            My Account
+          </h6>
+          <div className="pl-10">
+            <ul className="space-y-8 pt-10">
+              <li className="flex items-center text-sm space-x-4 cursor-pointer hover:text-gray-600">
+                <Image src={profile} width={20} height={20} alt="profile" />
+                <Link href="#"> Edit Profile </Link>
+              </li>
+              <li className="flex items-center text-sm space-x-4 cursor-pointer hover:text-gray-600">
+                <Image src={wallet} width={20} height={20} alt="profile" />
+                <Link href="#"> My Wallet </Link>
+              </li>
+              <li className="flex items-center text-sm space-x-4 cursor-pointer hover:text-gray-600">
+                <BsCartDash size={20} />
+                <Link href="#"> Orders </Link>
+              </li>
+              <li className="flex items-center text-sm  space-x-4 cursor-pointer hover:text-gray-600">
+                <Image src={saved} width={20} height={20} alt="profile" />
+                <Link href="#"> Saved Items </Link>
+              </li>
+              <li className="flex items-center text-sm  space-x-4 cursor-pointer hover:text-gray-600">
+                <Image src={purchase} width={20} height={20} alt="profile" />
+                <Link href="#"> Purchase History </Link>
+              </li>
+              <li className="flex items-center text-sm  space-x-4 cursor-pointer hover:text-gray-600">
+                <BsCartDash size={20} />
+                <Link href="#"> Pending Deals </Link>
+              </li>
+              <li className="flex items-center text-sm  space-x-4 cursor-pointer hover:text-gray-600">
+                <BsCartDash size={20} />
+                <Link href="#"> Succesful Deals</Link>
+              </li>
+            </ul>
+          </div>
+          <h6 className="mt-8 py-4 font-bold text-lg pl-10 text-white bg-brightRed">
+            Additional Info
+          </h6>
+          <div className="pl-10">
+            <ul className="space-y-8 pt-10">
+              <li className="flex items-center text-sm space-x-4 cursor-pointer hover:text-gray-600">
+                <Image src={thumb} width={20} height={20} alt="profile" />
+                <Link href="#"> Endorsed Business </Link>
+              </li>
+              <li className="flex items-center  text-sm space-x-4 cursor-pointer hover:text-gray-600">
+                <Image src={report} width={20} height={20} alt="profile" />
+                <Link href="#"> Report a Vendor </Link>
+              </li>
+              <li className="flex items-center text-sm space-x-4 cursor-pointer hover:text-gray-600">
+                <Image src={profile} width={20} height={20} alt="profile" />
+                <Link href="#"> Disputes </Link>
+              </li>
+            </ul>
+          </div>
+        </aside>
+      )}
+    </>
+  );
+};
 
-    return (
-        <>
-            {/* Mobile Menu sidebar */}
-            {
-                showSideBar && (
-                    <aside className="w-1/2 bg-white z-30 flex flex-col absolute top-0 left-0 shadow-md h-full md:hidden">
-                        <h6 className="py-4 font-bold text-lg pl-10 text-white bg-brightRed">My Account</h6>
-                        <div className="pl-10">
-                            <ul className="space-y-8 pt-10">
-                                <li className="flex items-center text-sm space-x-4 cursor-pointer hover:text-gray-600">
-                                    <Image src={profile} width={20} height={20} alt="profile" />
-                                    <Link href="#"> Edit Profile </Link>
-                                </li>
-                                <li className="flex items-center text-sm space-x-4 cursor-pointer hover:text-gray-600">
-                                    <Image src={wallet} width={20} height={20} alt="profile" />
-                                    <Link href="#"> My Wallet </Link>
-                                </li>
-                                <li className="flex items-center text-sm space-x-4 cursor-pointer hover:text-gray-600">
-                                    <BsCartDash size={20} />
-                                    <Link href="#"> Orders </Link>
-                                </li>
-                                <li className="flex items-center text-sm  space-x-4 cursor-pointer hover:text-gray-600">
-                                    <Image src={saved} width={20} height={20} alt="profile" />
-                                    <Link href="#"> Saved Items </Link>
-                                </li>
-                                <li className="flex items-center text-sm  space-x-4 cursor-pointer hover:text-gray-600">
-                                    <Image src={purchase} width={20} height={20} alt="profile" />
-                                    <Link href="#"> Purchase History </Link>
-                                </li>
-                                <li className="flex items-center text-sm  space-x-4 cursor-pointer hover:text-gray-600">
-                                    <BsCartDash size={20} />
-                                    <Link href="#"> Pending Deals </Link>
-                                </li>
-                                <li className="flex items-center text-sm  space-x-4 cursor-pointer hover:text-gray-600">
-                                    <BsCartDash size={20} />
-                                    <Link href="#"> Succesful Deals</Link>
-                                </li>
-                            </ul>
-                        </div>
-                        <h6 className="mt-8 py-4 font-bold text-lg pl-10 text-white bg-brightRed">
-                            Additional Info
-                        </h6>
-                        <div className="pl-10">
-                            <ul className="space-y-8 pt-10">
-                                <li className="flex items-center text-sm space-x-4 cursor-pointer hover:text-gray-600">
-                                    <Image src={thumb} width={20} height={20} alt="profile" />
-                                    <Link href="#"> Endorsed Business </Link>
-                                </li>
-                                <li className="flex items-center  text-sm space-x-4 cursor-pointer hover:text-gray-600">
-                                    <Image src={report} width={20} height={20} alt="profile" />
-                                    <Link href="#"> Report a Vendor </Link>
-                                </li>
-                                <li className="flex items-center text-sm space-x-4 cursor-pointer hover:text-gray-600">
-                                    <Image src={profile} width={20} height={20} alt="profile" />
-                                    <Link href="#"> Disputes </Link>
-                                </li>
-                            </ul>
-                        </div>
-                    </aside>
-                )
-            }
-        </>
-    )
-}
-
-export default MobileSidebar
+export default MobileSidebar;
--- components/BuyerNav/Navbar.jsx
+++ components/BuyerNav/Navbar.jsx
@@ -1,44 +1,50 @@
-import React from 'react'
-import Image from "next/image"
-import logo from "../../public/buyersImg/logo.svg"
-import arrow from "../../public/buyersImg/arrow.svg"
-import bell from "../../public/buyersImg/bell.svg"
-import profile from "../../public/buyersImg/profile.svg"
-import Search from './Search'
-
+import React from "react";
+import Image from "next/image";
+import logo from "../../public/buyersImg/logo.svg";
+import arrow from "../../public/buyersImg/arrow.svg";
+import bell from "../../public/buyersImg/bell.svg";
+import profile from "../../public/buyersImg/profile.svg";
+import Search from "./Search";
 
 const Navbar = ({ showbar }) => {
-
-    return (
-        <div className="customNavbar mx-auto relative w-full font-sans">
-            <nav className="p-2 mx-auto shadow-md hover:shadow-lg">
-                {/* flex Container */}
-                <div className="flex items-center justify-between px-10 md:px-14 md:space-x-10">
-                    <svg className="flex mt-2 items-center cursor-pointer md:hidden" fill="#491546" viewBox="0 0 100 80" width="40" height="40"
-                        onClick={showbar}>
-                        <rect width="100" height="10"></rect>
-                        <rect y="30" width="100" height="10"></rect>
-                        <rect y="60" width="100" height="10"></rect>
-                    </svg>
-                    <Image src={logo} className="h-2" alt="logo" />
-                    <Search />
-                    <div className="flex items-center space-x-6">
-                        <button className="text-sm p-1 text-brightRed bg-white rounded-lg flex gap-2 items-center
-                                 border-solid border-2 border-red-500  md:px-6 md:p-2 hover:bg-red-500 hover:text-white">
-                            <span>Pay Invoice</span>
-                            <Image src={arrow} width={10} height={10} alt="arrow" />
-                        </button>
-                        <div className="hidden  md:block md:mt-2">
-                            <Image src={bell} width={32} height={32} alt="bell" />
-                        </div>
-                        <div className="mt-2">
-                            <Image src={profile} width={32} height={32} alt="pics" />
-                        </div>
-                    </div>
-                </div>
-            </nav>
+  return (
+    <div className="customNavbar mx-auto relative w-full font-sans">
+      <nav className="p-2 mx-auto shadow-md hover:shadow-lg">
+        {/* flex Container */}
+        <div className="flex items-center justify-between px-10 md:px-14 md:space-x-10">
+          <svg
+            className="flex mt-2 items-center cursor-pointer md:hidden"
+            fill="#491546"
+            viewBox="0 0 100 80"
+            width="40"
+            height="40"
+            onClick={showbar}
+          >
+            <rect width="100" height="10"></rect>
+            <rect y="30" width="100" height="10"></rect>
+            <rect y="60" width="100" height="10"></rect>
+          </svg>
+          <Image src={logo} className="h-2" alt="logo" />
+          <Search />
+          <div className="flex items-center space-x-6">
+            <button
+              className="text-sm p-1 text-brightRed bg-white rounded-lg flex gap-2 items-center
+                                 border-solid border-2 border-red-500  md:px-6 md:p-2 hover:bg-red-500 hover:text-white"
+            >
+              <span>Pay Invoice</span>
+              <Image src={arrow} width={10} height={10} alt="arrow" />
+            </button>
+            <div className="hidden  md:block md:mt-2">
+              <Image src={bell} width={32} height={32} alt="bell" />
+            </div>
+            <div className="mt-2">
+              <Image src={profile} width={32} height={32} alt="pics" />
+            </div>
+          </div>
         </div>
-    )
-}
+      </nav>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
