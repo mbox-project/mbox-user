c01363504cf44338c83518f988d58e621730a819
Prudence2020 <uzoekwelevisonc@gmail.com>
2022-07-08 07:29:21 -0700

    added the header, newsletter and footer components

---
+++ pages/FAQSPage/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../../components/Header";
+import Newsletter from "../../components/Newsletter";
+import MainFooter from "../../components/MainFooter";
+const FAQSPage = () => {
+  return (
+    <section>
+      <Header />
+      <Newsletter />
+      <MainFooter />
+    </section>
+  );
+};
+export default FAQSPage;
