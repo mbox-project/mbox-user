8590206bced8dea067d866fa034cc87840f8bdbc
Prudence2020 <uzoekwelevisonc@gmail.com>
2022-09-07 16:53:43 -0700

    Worked on the search result page

--- components/Searchresult/Cribstore.jsx
+++ components/Searchresult/Cribstore.jsx
@@ -4,6 +4,8 @@ import cribsimg from "../../public/images/cribsimg.png";
 import starimg from "../../public/images/star.png";
 import thumbsupimg from "../../public/images/thumbsup.png";
 import locationimg from "../../public/images/location.png";
+import Button from "../Button";
+
 const cribStore = [
   {
     id: "C1",
@@ -20,44 +22,92 @@ const cribStore = [
 
 const Cribstore = () => {
   return (
-    <div>
-      <p className="font-poppins">Stores that contains the search word</p>
-      <div className="flex">
-        <div>
-          <Image src={cribsimg} />
+    <div className="bg-deepGray">
+      <p className="font-poppins ml-20 py-8 store text-lg ">
+        Stores that contains the search word
+      </p>
+      <div className="flex crib  pt-12  border-2 mx-20 rounded-md">
+        <div className="ml-12">
+          <Image width={400} height={300} src={cribsimg} />
         </div>
         <div>
           {cribStore.map((card) => {
             return (
-              <div key={card.id}>
-                <p>{card.name}</p>
-                <p>{card.body}</p>
-                <div className="flex">
-                  <div>
+              <div key={card.id} className="">
+                <p className="text-black text-2xl pt-2">{card.name}</p>
+                <p className="pt-4 max-w-xl text-sm leading-loose lightGrayColor">
+                  {card.body}
+                </p>
+                <div className="flex pt-5">
+                  <div className="flex pt-3">
                     <div>
                       <Image src={starimg} />
                     </div>
-                    <p>{card.starno}</p>
+                    <p className="pl-3">{card.starno}</p>
                   </div>
-                  <div>
-                    <div>
-                      <Image src={thumbsupimg} />
+                  <div className="flex">
+                    <div className="pl-3">
+                      <Image width={30} height={30} src={thumbsupimg} />
                     </div>
-                    <p>{card.thumbsupno}</p>
+                    <p className="pl-3 pt-3">{card.thumbsupno}</p>
                   </div>
-                  <div>
-                    <div>
+                  <div className="flex">
+                    <div className="pl-3">
                       <Image src={locationimg} />
                     </div>
-                    <p>{card.locationno}</p>
+                    <p className="pl-3 pt-3">{card.locationno}</p>
                   </div>
                 </div>
               </div>
             );
           })}
+          <Button className="w-full bg-orange-600 h-12 poppins  mb-0 lg:mb-4 rounded-lg text-white text-lg   mt-5">
+            Endorse Giveon’s Crib
+          </Button>
+        </div>
+      </div>
+      <div className="flex crib mt-10 pt-12  border-2 mx-20 rounded-md">
+        <div className="ml-12">
+          <Image width={400} height={300} src={cribsimg} />
+        </div>
+        <div >
+          {cribStore.map((card) => {
+            return (
+              <div key={card.id} className="">
+                <p className="text-black text-2xl pt-2">{card.name}</p>
+                <p className="pt-4 max-w-xl text-sm leading-loose lightGrayColor">
+                  {card.body}
+                </p>
+                <div className="flex pt-5">
+                  <div className="flex pt-3">
+                    <div>
+                      <Image src={starimg} />
+                    </div>
+                    <p className="pl-3">{card.starno}</p>
+                  </div>
+                  <div className="flex">
+                    <div className="pl-3">
+                      <Image width={30} height={30} src={thumbsupimg} />
+                    </div>
+                    <p className="pl-3 pt-3">{card.thumbsupno}</p>
+                  </div>
+                  <div className="flex">
+                    <div className="pl-3">
+                      <Image  src={locationimg} />
+                    </div>
+                    <p className="pl-3 pt-3">{card.locationno}</p>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+          <Button className="w-full bg-orange-600 h-12 poppins  mb-0 lg:mb-4 rounded-lg text-white text-lg   mt-5">
+            Endorse Giveon’s Crib
+          </Button>
         </div>
       </div>
     </div>
+    
   );
 };
 export default Cribstore;
