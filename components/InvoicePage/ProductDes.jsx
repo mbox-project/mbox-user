c6c89d25a8aa93d3080c37537f6e7f78db41c969
Prudence2020 <uzoekwelevisonc@gmail.com>
2023-02-04 12:09:23 -0800

    added the pay invoice page/modal

--- components/InvoicePage/ProductDes.jsx
+++ components/InvoicePage/ProductDes.jsx
@@ -30,6 +30,57 @@ const ProductDes = () => {
           </div>
           <hr></hr>
         </div>
+        <div>
+          <div className="flex justify-around  py-8">
+            <p>3</p>
+            <p>Air force II,skando Limited Edition</p>
+            <p>$40.00</p>
+            <p>5</p>
+            <p>$80.00</p>
+          </div>
+          <hr></hr>
+        </div>
+        <div>
+          <div className="flex justify-around  py-8">
+            <p>4</p>
+            <p>Air force II,skando Limited Edition</p>
+            <p>$40.00</p>
+            <p>5</p>
+            <p>$80.00</p>
+          </div>
+          <hr></hr>
+        </div>
+        <div>
+          <div className="flex justify-around  py-8">
+            <p>5</p>
+            <p>Air force II,skando Limited Edition</p>
+            <p>$40.00</p>
+            <p>5</p>
+            <p>$80.00</p>
+          </div>
+          <hr></hr>
+        </div>
+      </div>
+      <div className="font-poppins px-10 text-base pt-5 text-extrabold ">
+        <p className="flex justify-end">Subtotal:</p>
+        <p className="flex justify-end">Escrow fee:</p>
+      </div>
+      <div className="bg-brightRed ">
+        <p className=" py-2 px-10 mt-4   flex justify-end font-poppins text-base md:text-2xl lg:text-xl">
+          Total :
+        </p>
+      </div>
+      <div className="pt-6 px-6 font-poppins">
+        <p className="text-brightRed text-base text-bold">
+          {" "}
+          Terms $ Conditions
+        </p>
+        <p className="text-xs w-64  pt-3 leading-5">
+          Open your eyes well and don't go and do giveaweay with your money. A
+          word is enough for the wise.
+        </p>
+
+        <p className="py-6 text-extrabold">THANK YOU FOR TRUSTING US!</p>
       </div>
     </>
   );
