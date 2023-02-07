e594c5abd5955915ab492daac6bcaf40294ee315
Sikiru razak <sikirurazak1@gmail.com>
2023-02-07 07:17:05 -0800

    Merge branch 'gideon' of https://github.com/mbox-project/mbox-user into gideon

---
+++ pages/admin/admindashboard/index.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import Layout from "../../../components/AdminPagesLayout/Layout";
+
+const index = () => {
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const { isMerchant } = useSelector((state) => state.user);
+  return (
+    <>
+      <Layout>
+        <h2 className="card text-2xl border-b-2 mt-8 rectCard">Dashboard admin</h2>
+      </Layout>
+    </>
+  );
+};
+
+export default index;
