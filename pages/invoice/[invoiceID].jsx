e8c80b2a26992a1817ece25f437ac30b60e059a1
Sikiru razak <sikirurazak1@gmail.com>
2023-03-02 07:24:29 -0800

    test deployment

--- pages/invoice/[invoiceID].jsx
+++ pages/invoice/[invoiceID].jsx
@@ -13,50 +13,50 @@ import invoiceService from "../../store/invoice/invoiceService";
 const invoiceID = ({ data }) => {
   const router = useRouter();
   const dispatch = useDispatch();
-  // const invoiceID = router.query.invoiceID;
-  // const [data, setData] = useState(null);
-  // useEffect(() => {
-  //   dispatch(getInvoice(invoiceID)).then((action) => {
-  //     console.log(action);
-  //     setData(action.payload.data);
-  //   });
-  // }, []);
-  // const { isLoading } = useSelector((state) => state.invoice);
+  const invoiceID = router.query.invoiceID;
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    dispatch(getInvoice(invoiceID)).then((action) => {
+      console.log(action);
+      setData(action.payload.data);
+    });
+  }, []);
+  const { isLoading } = useSelector((state) => state.invoice);
   return (
-    <div>
-      <Header />
-      <div className="flex poppins pl-24 pt-5">
-        <p>Home </p>
-        <p className="pl-1">invoice</p>
-      </div>
-      <Edit />
-      {data && <Receipt data={data} />}
-      <MainFooter />
-    </div>
-    // <>
-    //   {isLoading ? (
-    //     <Spinner />
-    //   ) : (
-    //     <div>
-    //       <Header />
-    //       <div className="flex poppins pl-24 pt-5">
-    //         <p>Home </p>
-    //         <p className="pl-1">invoice</p>
-    //       </div>
-    //       <Edit />
-    //       {data && <Receipt data={data} />}
-    //       <MainFooter />
-    //     </div>
-    //   )}
-    // </>
+    // <div>
+    //   <Header />
+    //   <div className="flex poppins pl-24 pt-5">
+    //     <p>Home </p>
+    //     <p className="pl-1">invoice</p>
+    //   </div>
+    //   <Edit />
+    //   {data && <Receipt data={data} />}
+    //   <MainFooter />
+    // </div>
+    <>
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        <div>
+          <Header />
+          <div className="flex poppins pl-24 pt-5">
+            <p>Home </p>
+            <p className="pl-1">invoice</p>
+          </div>
+          <Edit />
+          {data && <Receipt data={data} />}
+          <MainFooter />
+        </div>
+      )}
+    </>
   );
 };
 export default invoiceID;
 
-export async function getServerSideProps(context) {
-  const id = context.params.invoiceID;
-  const data = invoiceService.getInvoice(id);
-  return {
-    props: { data },
-  };
-}
+// export async function getServerSideProps(context) {
+//   const id = context.params.invoiceID;
+//   const data = invoiceService.getInvoice(id);
+//   return {
+//     props: { data },
+//   };
+// }
