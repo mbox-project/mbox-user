0b68057d89d5d1eb412ac52a401fd8fcef5dd4a1
Adeolu Akanni <akanniadeolu9@gmail.com>
2024-01-31 17:15:42 +0100

    Endorsed

--- pages/endorse/index.jsx
+++ pages/endorse/index.jsx
@@ -1,6 +1,5 @@
-import React, {useState} from "react";
-import Sidebar from "../../components/PagesLayout/Sidebar";
-import Navbar from "../../components/PagesLayout/Navbar";
+import React, { useState } from "react";
+import Layout from "../../components/PagesLayout/Layout";
 import Input from "../../components/Input";
 import Label from "../../components/Label";
 import Button from "../../components/Button";
@@ -9,11 +8,10 @@ import baseURL from "../../config/api";
 import { toastify } from "../../helpers";
 
 const endorse = () => {
-
   const dispatch = useDispatch();
   const [endorseData, setData] = useState({
     storeName: "",
-    comment: ""
+    comment: "",
   });
 
   const onChangeInput = (e) => {
@@ -27,38 +25,37 @@ const endorse = () => {
     e.preventDefault();
 
     const response = await fetch(`${baseURL}Endorsement/endorse`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${typeof window !== 'undefined' ? sessionStorage.getItem('token') : null}`,
-        },
-        body: JSON.stringify(endorseData),
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${
+          typeof window !== "undefined" ? sessionStorage.getItem("token") : null
+        }`,
+      },
+      body: JSON.stringify(endorseData),
     });
 
     if (response.ok) {
-        const result = await response.text();
-        toastify.alertSuccess(result); 
+      const result = await response.text();
+      toastify.alertSuccess(result);
     } else {
-        const errorResult = await response.text();
-        toastify.alertError(errorResult); 
+      const errorResult = await response.text();
+      toastify.alertError(errorResult);
     }
-};
-
-
+  };
 
   return (
-    <div>
-      <Navbar />
-      <div className="flex bg-[#FAFAFA]">
-        <Sidebar />
-        <div className="w-full  mx-20">
-          <div className=" mt-10">
-            <p className="bg-white  pt-5 pb-2 px-4 poppins rounded-sm border-b">
-              Endorse a store{" "}
-            </p>
+    <>
+      <Layout>
+        <section className="card rectCard flex justify-between items-center !bg-[#FAFAFA] text-lg border-b-2 mt-8 md:flex-row ">
+          <div className="mt-5">
+            <h4 className="text-xl font-medium">Endorse a Store</h4>
           </div>
+        </section>
+
+        <section className="!bg-[#FAFAFA]">
           <form onSubmit={handleEndorseForm}>
-            <div className="bg-white  pt-5 pb-2 px-4 poppins rounded-sm">
+            <div className="py-5 poppins rounded-sm">
               {/* <div className=" px-4 pt-3">
                 <Label
                   className="text-[#C1C1C1]  text-sm"
@@ -83,7 +80,7 @@ const endorse = () => {
                   name="storeName"
                   type="text"
                   placeHolder="Giveon's Crib"
-                  className="w-full p-1 md:p-2 lg:py-2  focus:outline-none pr-12 text-lg lg:text-sm  font-poppins  mt-2 border-[#9F9F9F] border-1 bg-white md:border-2  md:rounded-md shadow-sm rounded-none"
+                  className="w-full p-1 md:p-2 lg:py-2  focus:outline-none pr-12 text-lg lg:text-sm  font-poppins  mt-2 border-[#9F9F9F] border-1 md:border-2  md:rounded-md shadow-sm rounded-none"
                   //value={storeName}
                   onChange={onChangeInput}
                   required
@@ -95,61 +92,62 @@ const endorse = () => {
                   htmlFor="text"
                   title="Comment (Optional)"
                 />
-                <Input
+                <textarea
                   name="comment"
                   type="text"
                   placeHolder=""
                   onChange={onChangeInput}
-                  className="w-full p-1 md:p-2 lg:py-12  focus:outline-none pr-12 text-lg lg:text-sm  font-poppins  mt-2 border-[#9F9F9F] border-1 bg-white md:border-2  md:rounded-md shadow-sm rounded-none"
+                  className="w-full p-1 md:p-2 lg:py-12  focus:outline-none pr-12 text-lg lg:text-sm  font-poppins  mt-2 border-[#9F9F9F] border-1 md:border-2  md:rounded-md shadow-sm rounded-none"
                   required
                 />
               </div>
-              <Button type="submit" className=" w-24 my-4 rounded-md shadow-lg bg-brightRed  py-2  text-white relative left-96 text-base poppins">
-                Submit
-              </Button>
+              <div className="flex justify-end px-5">
+                <Button
+                  type="submit"
+                  className=" w-24 my-4 rounded-md shadow-lg bg-brightRed  py-2  text-white left-96 text-base poppins"
+                >
+                  Submit
+                </Button>
+              </div>
             </div>
           </form>
+        </section>
+
+        <div>
+          <section className="card rectCard flex justify-between items-center text-[#26A17B] !bg-[#FAFAFA] text-lg border-b-2 mt-8">
+            <div className="mt-5">
+              <h4 className="text-xl font-medium">Endorsed Businesses</h4>
+            </div>
+            <form>
+              <select
+                id="sort"
+                className="border border-[#26A17B] text-sm rounded-md p-2.5 px-6 mt-5"
+              >
+                <option disabled selected>
+                  Sort By
+                </option>
+                <option value="d">date</option>
+                <option value="c">type</option>
+              </select>
+            </form>
+          </section>
+
+          <section className="flex flex-col bg-[#FAFAFA] px-5 py-5 space-y-5">
+            <div className="flex justify-between py-2 text-[#9A9A9A] border-b-2">
+              <div>Store Name</div>
+              <div>Date & Time</div>
+              <div>Comments</div>
+            </div>
+
+            <div className="flex justify-between text-sm">
+              <div>Giveons Crib</div>
+              <div>09.01.2022,06:34am</div>
+              <div>Comments</div>
+            </div>
+          </section>
         </div>
-      </div>
-      <div>
-        <div className="flex justify-between pl-24 pt-5  text-sm poppins ">
-          <p className="flex text-[#76C2AA]">Endorse Businesses</p>
-          <form>
-            <select
-              id="sort"
-              className="bg-gray-50 border text-gray-500 text-sm rounded-md p-2.5 px-6 mt-5"
-            >
-              <option disabled selected>
-                Sort By
-              </option>
-              <option value="d">price</option>
-              <option value="c">size</option>
-            </select>
-          </form>
-          <hr />
-        </div>
-        <div className="flex justify-between">
-          <div>Store Name</div>
-          <div>Date & Time</div>
-          <div> Reason</div>
-        </div>
-        <div className="flex justify-between">
-          <div>Giveons Crib</div>
-          <div>09.01.2022,06:34am</div>
-          <div> Reason</div>
-        </div>
-        <div className="flex justify-between">
-          <div>Giveons Crib</div>
-          <div>09.01.2022,06:34am</div>
-          <div> Reason</div>
-        </div>
-        <div className="flex justify-between">
-          <div>Giveons Crib</div>
-          <div>09.01.2022,06:34am</div>
-          <div> Reason</div>
-        </div>
-      </div>
-    </div>
+      </Layout>
+    </>
   );
 };
 export default endorse;
