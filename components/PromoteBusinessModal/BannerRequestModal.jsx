b9a760e8ac1e14fc3d845c927c50d2858f6bdd9f
Stephen Adeniji <praiseadeniji2017@gmail.com>
2024-04-04 08:04:44 +0100

    feat: promote business page and modals

---
+++ components/PromoteBusinessModal/BannerRequestModal.jsx
@@ -0,0 +1,131 @@
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { Modal, DatePicker } from "antd";
+
+import Input from "../Input";
+import Label from "../Label";
+import { useRouter } from "next/router";
+
+const BannerRequestModal = ({ open, setOpen }) => {
+  return (
+    <Modal
+      open={open}
+      onCancel={() => setOpen(false)}
+      footer={null}
+      className="!p-0"
+    >
+      <div className="border rounded-md shadow-lg">
+        <div className="bg-brightRed ">
+          <p className=" py-4 px-4 text-white font-poppins text-xl">
+            Banner request form
+          </p>
+        </div>
+        <div className="bg-lightPink ">
+          <p className=" py-3 px-4 font-poppins text-xs text-[#F16A2D]">
+            Boost your visibility with a banner! Simply fill out the form and
+            our customer care team will get back to you shortly. Only $1 per
+            day.
+          </p>
+        </div>
+        <div className="bg-[#FAFAFA]">
+          <div className=" px-4 pt-4">
+            <Label
+              className="text-[#444444] font-medium md:text-base text-sm"
+              htmlFor="days"
+              title="Number of days"
+            />
+            <Input
+              name="days"
+              type="number"
+              placeHolder="4"
+              className="w-full p-1 md:p-2 lg:py-2  focus:outline-none pr-12 text-lg lg:text-sm  font-poppins  mt-2 border-[#9F9F9F] bg-[#FAFAFA] border-1  md:border-2  md:rounded-md shadow-sm rounded-none"
+              required
+            />
+          </div>
+          <div className=" px-4 pt-2">
+            <Label
+              className="text-[#444444] font-medium md:text-base text-sm"
+              htmlFor="starts"
+              title="Starts on:"
+            />
+            <DatePicker
+              name="starts"
+              required
+              className="block !py-2 mt-2 !bg-transparent !border-1 border-[#9F9F9F] text-brightRed hover:border-brightRed focus:border-brightRed"
+            />
+          </div>
+          <div className=" px-4 pt-2">
+            <Label
+              className="text-[#444444] font-medium md:text-base text-sm"
+              htmlFor="ends"
+              title="Ends on:"
+            />
+            <DatePicker
+              name="ends"
+              required
+              className="block !py-2 mt-2 !bg-transparent !border-1 border-[#9F9F9F] text-brightRed hover:border-brightRed focus:border-brightRed"
+            />
+          </div>
+          <div className=" px-4 pt-3">
+            <Label
+              className="text-[#444444] font-medium md:text-base text-sm"
+              htmlFor="price"
+              title="Total price:"
+            />
+            <Input
+              name="price"
+              type="text"
+              placeHolder="$1,000"
+              className="w-full p-1 md:p-2 lg:py-2  focus:outline-none pr-12 text-lg lg:text-sm  font-poppins  mt-2 border-[#9F9F9F] border-1 bg-[#FAFAFA] md:border-2  md:rounded-md shadow-sm rounded-none"
+              required
+            />
+          </div>
+
+          <p className=" font-poppins font-semibold text-sm text-[#717171] px-4 pt-4 ">
+            Select Payment Option
+          </p>
+          <div className="flex justify-between pt-4 px-[5%]">
+            <div className="flex items-center">
+              <input type="checkbox" />
+              <p className="px-5 text-xs poppins pt-1 text-semibold">
+                MBOX-Wallet -$3,050
+              </p>
+            </div>
+            <div className="flex justify-between">
+              <button
+                className="w-full flex gap-2 my-4 rounded-md shadow-lg bg-[#034694] p-[0.5rem] text-[#FAFAFA] text-base poppins"
+                type="submit"
+              >
+                <svg
+                  width="25"
+                  height="25"
+                  viewBox="0 0 25 25"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M16.224 12.7952C16.224 13.0528 16.0151 13.2619 15.7573 13.2619H12.7783V16.2411C12.7783 16.4989 12.5692 16.7078 12.3116 16.7078C12.054 16.7078 11.8449 16.4989 11.8449 16.2411V13.2619H8.86529C8.60767 13.2619 8.3986 13.0528 8.3986 12.7952C8.3986 12.5376 8.60767 12.3285 8.86529 12.3285H11.8449V9.34929C11.8449 9.09167 12.054 8.8826 12.3116 8.8826C12.5692 8.8826 12.7783 9.09167 12.7783 9.34929V12.3287H15.7573C16.0151 12.3285 16.224 12.5376 16.224 12.7952V12.7952ZM20.2447 12.7952C20.2447 14.914 19.4194 16.9063 17.9211 18.4045C16.3745 19.9511 14.3431 20.7244 12.3118 20.7244C10.2805 20.7244 8.2491 19.9512 6.70249 18.4045C5.20421 16.9062 4.37891 14.9139 4.37891 12.7952C4.37891 10.676 5.20421 8.68411 6.70249 7.18588C8.20076 5.68761 10.1931 4.8623 12.3118 4.8623C14.4305 4.8623 16.4229 5.68761 17.9211 7.18588C19.4194 8.68397 20.2447 10.6761 20.2447 12.7952ZM19.3113 12.7952C19.3113 10.9255 18.5833 9.16752 17.2611 7.84538C15.9392 6.52324 14.1813 5.79522 12.3113 5.79522C10.4414 5.79522 8.68365 6.52324 7.36151 7.84538C6.03937 9.16752 5.31135 10.9253 5.31135 12.7952C5.31135 14.6649 6.03937 16.4229 7.36151 17.745C10.0909 20.4739 14.5317 20.4742 17.2607 17.745C18.5828 16.4227 19.3108 14.6651 19.3108 12.7952H19.3113Z"
+                    fill="#FAFAFA"
+                    stroke="#FAFAFA"
+                    stroke-width="0.793103"
+                  />
+                </svg>
+                Fund Wallet
+              </button>
+            </div>
+          </div>
+
+          <div className="px-4">
+            <button
+              className="w-full my-4 rounded-md shadow-lg bg-brightRed py-[0.5rem] text-white text-base poppins"
+              type="submit"
+            >
+              Submit
+            </button>
+          </div>
+        </div>
+      </div>
+    </Modal>
+  );
+};
+export default BannerRequestModal;
--- pages/promotebusiness/index.jsx
+++ pages/promotebusiness/index.jsx
@@ -1,108 +1,191 @@
-import React from "react";
-import Input from "../../components/Input";
-import Label from "../../components/Label";
-import Button from "../../components/Button";
+import React, { useState } from "react";
+import Link from "next/link";
+import Image from "next/image";
+import Layout from "../../components/PagesLayout/Layout";
+import Pagenation from "../../components/Pagenation";
+import shorts from "../../public/img/shorts.png";
+import BannerRequestModal from "../../components/PromoteBusinessModal/BannerRequestModal";
+import CategoryListingModal from "../../components/PromoteBusinessModal/CategoryListingModal";
+import { BiChevronDown } from "react-icons/bi";
+
+const data = [
+  {
+    id: 1,
+    offer: "Banner Request",
+    type: "Store Advert",
+    duration: "11th - 25th Feb ( 2 weeks)",
+  },
+  {
+    id: 2,
+    offer: "Category Listing",
+    type: "Landing Page",
+    duration: "11th - 25th Feb ( 2 weeks)",
+  },
+  {
+    id: 3,
+    offer: "Banner Request",
+    type: "Store Advert",
+    duration: "11th - 25th Feb ( 2 weeks)",
+  },
+  {
+    id: 4,
+    offer: "Category Listing",
+    type: "Landing Page",
+    duration: "11th - 25th Feb ( 2 weeks)",
+  },
+  {
+    id: 5,
+    offer: "Banner Request",
+    type: "Store Advert",
+    duration: "11th - 25th Feb ( 2 weeks)",
+  },
+];
+
 const promoteBusiness = () => {
+  const [isBannerModalOpen, setIsBannerModalOpen] = useState(false);
+  const [isCategoryListingModalOpen, setIsCategoryListingModalOpen] =
+    useState(false);
+
   return (
-    <div className=" border rounded-md lg:mx-96 my-20 shadow-lg">
-      <div className="bg-brightRed ">
-        <p className=" py-4 px-4 text-white font-poppins text-xl">
-          Suggested Store Form
-        </p>
-      </div>
-      <div className="bg-lightPink ">
-        <p className=" py-3 px-4 font-poppins text-xs text-[#F16A2D]">
-          Dear Merchant, please provide the following information correctly
-        </p>
-      </div>
-      <div className="bg-[#FAFAFA]">
-        <div className=" px-4 pt-4">
-          <Label
-            className="text-[#C1C1C1] text-sm"
-            htmlFor="text"
-            title="Duration"
-          />
-          <Input
-            name="tag"
-            type="text"
-            placeHolder=""
-            className="w-full p-1 md:p-2 lg:py-2  focus:outline-none pr-12 text-lg lg:text-sm  font-poppins  mt-2 border-[#9F9F9F] bg-[#FAFAFA] border-1  md:border-2  md:rounded-md shadow-sm rounded-none"
-            required
-          />
-        </div>
-        <div className=" px-4 pt-3">
-          <Label
-            className="text-[#C1C1C1]  text-sm"
-            htmlFor="text"
-            title="Listing Type"
-          />
-          <Input
-            name="issuer"
-            type="text"
-            placeHolder=""
-            className="w-full p-1 md:p-2 lg:py-2  focus:outline-none pr-12 text-lg lg:text-sm  font-poppins  mt-2 border-[#9F9F9F] border-1 bg-[#FAFAFA] md:border-2  md:rounded-md shadow-sm rounded-none"
-            required
-          />
-        </div>
-        <div className=" px-4 pt-2">
-          <Label
-            className="text-[#C1C1C1] text-sm"
-            htmlFor="text"
-            title="ts on:"
-          />
-          <Input
-            name="issuer"
-            type="text"
-            placeHolder=""
-            className="w-full p-1 md:p-2 lg:py-2  focus:outline-none pr-12 text-lg lg:text-sm  font-poppins  mt-2 border-[#9F9F9F] border-1 bg-[#FAFAFA]  md:border-2  md:rounded-md shadow-sm rounded-none"
-            required
-          />
-        </div>
-        <div className=" px-4 pt-2">
-          <Label
-            className="text-[#C1C1C1] text-sm"
-            htmlFor="text"
-            title="Ends on:"
-          />
-          <Input
-            name="issuer"
-            type="text"
-            placeHolder=""
-            className="w-full p-1 md:p-2 lg:py-2  focus:outline-none pr-12 text-lg lg:text-sm  font-poppins  mt-2 border-[#9F9F9F] border-1 bg-[#FAFAFA] md:border-2  md:rounded-md shadow-sm rounded-none"
-            required
-          />
-        </div>
+    <Layout>
+      <main className="">
+        <section>
+          <div className="card rectCard flex md:!px-8 px-4 justify-between items-center text-lg border-b-2 mt-8 md:flex-row ">
+            <h4 className="text-2xl font-medium mt-5">Promote Business</h4>
+            <form>
+              <select
+                id="sort"
+                className="bg-gray-50 border text-gray-500 rounded-md p-2.5 px-6 mt-5"
+              >
+                <option disabled selected>
+                  Sort By
+                </option>
+                <option value="offer">Offer</option>
+                <option value="type">Type</option>
+              </select>
+            </form>
+          </div>
+          {/*  */}
+          <div className="card rectCard md:!px-8 !px-4 !pb-16 !rounded-b-xl overflow-x-auto">
+            <div className="flex flex-col gap-5">
+              <button
+                type="button"
+                onClick={() => setIsBannerModalOpen(true)}
+                className="flex justify-between gap-8 items-center border-b-2 border-[#9A9A9A1A] py-3 text-[#444444] text-[20px]"
+              >
+                Request a banner
+                <svg
+                  width="12"
+                  height="20"
+                  viewBox="0 0 12 20"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M1.44927 1.24224C1.14855 1.54296 1.12121 2.01355 1.36725 2.34516L1.44927 2.44016L8.75581 9.74708L1.44927 17.054C1.14855 17.3547 1.12121 17.8253 1.36725 18.1569L1.44927 18.2519C1.74999 18.5527 2.22058 18.58 2.55219 18.3339L2.64719 18.2519L10.5531 10.346C10.8538 10.0453 10.8811 9.57474 10.6351 9.24313L10.5531 9.14812L2.64719 1.24224C2.31639 0.911441 1.78007 0.911441 1.44927 1.24224Z"
+                    fill="#444444"
+                    stroke="#444444"
+                    stroke-width="1.5"
+                  />
+                </svg>
+              </button>
+              <BannerRequestModal
+                open={isBannerModalOpen}
+                setOpen={setIsBannerModalOpen}
+              />
 
-        <p className=" font-poppins font-semibold text-sm text-[#717171] px-4 pt-4 ">
-          Select Payment Option
-        </p>
-        <div className=" flex px-4 pt-4">
-          <div>
-            <input type="checkbox" />
+              <button
+                type="button"
+                onClick={() => setIsCategoryListingModalOpen(true)}
+                className="flex justify-between gap-8 items-center border-b-2 border-[#9A9A9A1A] py-3 text-[#444444] text-[20px]"
+              >
+                Request a category listing
+                <svg
+                  width="12"
+                  height="20"
+                  viewBox="0 0 12 20"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M1.44927 1.24224C1.14855 1.54296 1.12121 2.01355 1.36725 2.34516L1.44927 2.44016L8.75581 9.74708L1.44927 17.054C1.14855 17.3547 1.12121 17.8253 1.36725 18.1569L1.44927 18.2519C1.74999 18.5527 2.22058 18.58 2.55219 18.3339L2.64719 18.2519L10.5531 10.346C10.8538 10.0453 10.8811 9.57474 10.6351 9.24313L10.5531 9.14812L2.64719 1.24224C2.31639 0.911441 1.78007 0.911441 1.44927 1.24224Z"
+                    fill="#444444"
+                    stroke="#444444"
+                    stroke-width="1.5"
+                  />
+                </svg>
+              </button>
+              <CategoryListingModal
+                open={isCategoryListingModalOpen}
+                setOpen={setIsCategoryListingModalOpen}
+              />
+            </div>
           </div>
-          <div>
-            <p className="px-5 text-xs poppins pt-1 text-semibold">
-              MBOX-Wallet -$3,050
-            </p>
-          </div>
-          <div>
-            <Button className="bg-[#034694] relative left-72 rounded text-xs py-1.5 px-2 text-white">
-              Fund Wallet
-            </Button>
-          </div>
-        </div>
-        <div>
-          <p className="text-[#F16A2D] px-4 py-3 text-xs">
-            This offer would cost you a flat rate of #500 (five hundred naira
-            only)
-          </p>
-        </div>
+        </section>
 
-        <Button className=" w-full my-4 rounded-md shadow-lg bg-brightRed  py-2  text-white flex justify-center text-base poppins">
-          Submit
-        </Button>
-      </div>
-    </div>
+        {/* Business Activity */}
+        <section>
+          <div className="card rectCard md:!px-8 px-4 flex justify-between items-center text-lg border-b-2 mt-8 md:flex-row ">
+            <h4 className="text-2xl font-medium mt-5">Business Activity</h4>
+            <form>
+              <select
+                id="sort"
+                className="bg-gray-50 border text-gray-500 rounded-md p-2.5 px-6 mt-5"
+              >
+                <option disabled selected>
+                  Sort By
+                </option>
+                <option value="offer">Offer</option>
+                <option value="type">Type</option>
+              </select>
+            </form>
+          </div>
+          {/*  */}
+          <div className="card rectCard !pb-16 !rounded-b-xl overflow-x-auto">
+            <table className="w-full">
+              <thead className="px-6 py-4 border-b-2 border-[#9A9A9A1A] text-[#9A9A9A] font-medium text-[18px]">
+                <th className="py-4 px-6 text-start">Promotional Offer</th>
+                <th className="py-4 px-6 text-start">Type</th>
+                <th className="py-4 px-6 text-start">Duration</th>
+              </thead>
+              <tbody className="">
+                {data.map((activity) => (
+                  <tr key={activity.id}>
+                    <td className="px-6 py-4 whitespace-nowrap">
+                      <div className="text-[#444444] font-medium">
+                        {activity.offer}
+                      </div>
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap">
+                      <div className="text-[#444444] font-medium">
+                        {activity.type}
+                      </div>
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap">
+                      <div className="text-[#444444] font-medium">
+                        {activity.duration}
+                      </div>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+
+            {/* show more */}
+            <div className="py-6 px-4 mt-4 flex md:justify-end">
+              <button
+                type="button"
+                className="text-[#EF5612] text-[18px] flex items-center gap-2"
+              >
+                <p className="underline">Show more</p>
+                <BiChevronDown className="w-8 h-8" />
+              </button>
+            </div>
+          </div>
+        </section>
+      </main>
+    </Layout>
   );
 };
+
 export default promoteBusiness;
