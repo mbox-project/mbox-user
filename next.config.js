3d0efdb11a83ea97b320f590e16876e39463e56a
Stephen Adeniji <praiseadeniji2017@gmail.com>
2024-04-30 14:26:15 +0100

    fix: next-pwa

--- next.config.js
+++ next.config.js
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  // disable: process.env.NODE_ENV === 'development',
+});
+
+module.exports = withPWA({
+  // next.js config
   reactStrictMode: false,
   webpack5: true,
   eslint: {
@@ -7,14 +16,5 @@ const nextConfig = {
   },
   images: {
     domains: ["res.cloudinary.com"],
-  },
-};
-
-module.exports = withPWA({
-  ...nextConfig,
-  pwa: {
-    dest: "public",
-    register: true,
-    skipWaiting: true,
   },
 });
