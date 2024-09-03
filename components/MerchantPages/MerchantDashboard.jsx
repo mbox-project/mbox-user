fe04f09e4f3292fc995cdb05b4f716be122553fe
Stephen Adeniji <praiseadeniji2017@gmail.com>
2024-09-03 16:15:00 +0100

    feat: daily active buyers chart

--- components/MerchantPages/MerchantDashboard.jsx
+++ components/MerchantPages/MerchantDashboard.jsx
@@ -15,6 +15,7 @@ import { message } from "antd";
 import { getVendorDashboard } from "../../store/users/userService";
 import { FaXTwitter } from "react-icons/fa6";
 import { FiArrowDownRight, FiArrowUpRight } from "react-icons/fi";
+import DailyActiveBuyersChart from "./DailyActiveBuyersChart";
 
 const MerchantDashboard = () => {
   const dispatch = useDispatch();
@@ -120,78 +121,202 @@ const MerchantDashboard = () => {
             </div>
           </section>
 
-          <section className="px-2 sm:px-6 py-8 bg-white rounded-md space-y-6">
-            <h2 className="text-2xl font-semibold text-[#444444]">
-              Revenue Comparisons
-            </h2>
-            <div className="grid grid-cols-1 lg:grid-cols-2 gap-10">
-              <div className="p-6 rounded-3xl shadow-md shadow-[#49154612] bg-brightPurple flex flex-col gap-4 text-white">
-                <h3 className="text-white/75 text-xl sm:text-2xl">
-                  Weekly Revenue Generated
-                </h3>
-                <div className="grid sm:grid-cols-2 gap-5 sm:gap-10">
-                  <div className="space-y-3">
-                    <div className="py-6 px-4 space-y-2 bg-white bg-opacity-15 rounded-lg text-center">
-                      <h3 className="text-2xl md:text-4xl font-bold">14,766</h3>
-                      <h4>Naira</h4>
-                    </div>
-                    <div className="text-center text-lg">
-                      <p className="capitalize text-white/60 text-sm">
-                        This week
-                      </p>
-                      <span className="text-[#F90808] font-semibold flex items-center justify-center">
-                        3.65%
-                        <FiArrowDownRight className="size-6" />
-                      </span>
-                    </div>
-                  </div>
-                  <div className="space-y-3">
-                    <div className="py-6 px-4 space-y-2 bg-white bg-opacity-15 rounded-lg text-center">
-                      <h3 className="text-2xl md:text-4xl font-bold">15,766</h3>
-                      <h4>Naira</h4>
-                    </div>
-                    <div className="text-center text-lg">
-                      <p className="capitalize text-white/60 text-sm">
-                        last week
-                      </p>
-                      <span className="text-[#26A17B] font-semibold flex items-center justify-center">
-                        5.85%
-                        <FiArrowUpRight className="size-6" />
-                      </span>
+          <section className="px-2 sm:px-6 py-8 bg-white rounded-md space-y-4 md:space-y-12">
+            <div className="grid lg:grid-cols-2 gap-6 md:gap-12">
+              <div className="flex flex-col gap-8">
+                {/* Buyers Overview */}
+                <div className="space-y-4">
+                  <h2 className="text-2xl font-semibold text-[#444444]">
+                    Buyers Overview
+                  </h2>
+                  <div className="p-6 rounded-3xl shadow-xl shadow-[#03469412] bg-[#034694] flex flex-col gap-6 text-white">
+                    <h3 className="text-white/75 text-xl sm:text-2xl font-medium">
+                      Buyers maintenance overview
+                    </h3>
+                    <div className="grid sm:grid-cols-3 gap-4 gap-y-8 sm:gap-8">
+                      <div className="flex flex-col gap-6 items-center justify-between text-center">
+                        <Image
+                          src="/img/merchant-dashboard/calendar.svg"
+                          alt="calendar"
+                          width={45}
+                          height={44}
+                        />
+                        <div className="space-y-3">
+                          <h3 className="text-2xl md:text-4xl font-bold uppercase">
+                            47.6K
+                          </h3>
+
+                          <span className="text-[#F90808] text-xl md:text-2xl font-semibold flex gap-1 items-center justify-center">
+                            3.65%
+                            <FiArrowDownRight className="size-7" />
+                          </span>
+                        </div>
+                      </div>
+
+                      <div className="flex flex-col gap-6 items-center justify-between text-center">
+                        <Image
+                          src="/img/merchant-dashboard/week.svg"
+                          alt="calendar"
+                          width={115}
+                          height={22}
+                        />
+                        <div className="space-y-3">
+                          <h3 className="text-2xl md:text-4xl font-bold uppercase">
+                            290K
+                          </h3>
+
+                          <span className="text-[#26A17B] text-xl md:text-2xl font-semibold flex gap-1 items-center justify-center">
+                            5.85%
+                            <FiArrowUpRight className="size-7" />
+                          </span>
+                        </div>
+                      </div>
+
+                      <div className="flex flex-col gap-6 items-center justify-between text-center">
+                        <Image
+                          src="/img/merchant-dashboard/icon-group.svg"
+                          alt="calendar"
+                          width={45}
+                          height={44}
+                        />
+                        <div className="space-y-3">
+                          <h3 className="text-2xl md:text-4xl font-bold uppercase">
+                            770K
+                          </h3>
+
+                          <span className="text-[#26A17B] text-xl md:text-2xl font-semibold flex gap-1 items-center justify-center">
+                            8.34%
+                            <FiArrowUpRight className="size-7" />
+                          </span>
+                        </div>
+                      </div>
                     </div>
                   </div>
                 </div>
+
+                {/* Daily Active Buyers (chart) */}
+                <div className="p-6 rounded-3xl flex flex-col gap-6 shadow-xl text-[#444444]">
+                  <h3 className="text-[#444444] text-xl sm:text-2xl font-medium">
+                    Daily Active Buyers
+                  </h3>
+
+                  <DailyActiveBuyersChart />
+                </div>
               </div>
 
-              <div className="p-6 rounded-3xl flex flex-col gap-4 shadow-xl text-[#444444]">
-                <h3 className="text-[#444444] text-xl sm:text-2xl">
-                  Total Revenue Generated
-                </h3>
-                <div className="grid sm:grid-cols-2 gap-5 sm:gap-10">
-                  <div className="space-y-3">
-                    <div className="py-6 px-4 space-y-2 bg-[#EFEFEF] rounded-lg text-center">
-                      <h3 className="text-2xl md:text-4xl font-bold">14,766</h3>
-                      <h4>Naira</h4>
+              {/* Top Selling Items */}
+              <div className="space-y-4">
+                <h2 className="text-2xl font-semibold text-[#444444]">
+                  Top Selling Items
+                </h2>
+
+                <ul className="list-decimal list-inside flex flex-col gap-4 text-[#444444]">
+                  <li className="text-lg bg-[#FAFAFA] px-4 py-2 rounded-sm">
+                    Longshort Sneakers
+                  </li>
+                  <li className="text-lg bg-[#FAFAFA] px-4 py-2 rounded-sm">
+                    Modern XLE banzo El-caporal
+                  </li>
+                  <li className="text-lg bg-[#FAFAFA] px-4 py-2 rounded-sm">
+                    Mummy Bisola Skincare
+                  </li>
+                  <li className="text-lg bg-[#FAFAFA] px-4 py-2 rounded-sm">
+                    Warizi Bata tanatana
+                  </li>
+                  <li className="text-lg bg-[#FAFAFA] px-4 py-2 rounded-sm">
+                    Vero’s Massage Oil limited
+                  </li>
+                  <li className="text-lg bg-[#FAFAFA] px-4 py-2 rounded-sm">
+                    Think Like A Man Sanders Four
+                  </li>
+                  <li className="text-lg bg-[#FAFAFA] px-4 py-2 rounded-sm">
+                    Artificial Cargo Boxers
+                  </li>
+                </ul>
+              </div>
+            </div>
+
+            <div className="space-y-4">
+              <h2 className="text-2xl font-semibold text-[#444444]">
+                Revenue Comparisons
+              </h2>
+              <div className="grid grid-cols-1 lg:grid-cols-2 gap-10">
+                <div className="p-6 rounded-3xl shadow-xl shadow-[#49154612] bg-brightPurple flex flex-col gap-6 text-white">
+                  <h3 className="text-white/75 text-xl sm:text-2xl font-medium">
+                    Weekly Revenue Generated
+                  </h3>
+                  <div className="grid sm:grid-cols-2 gap-5 sm:gap-10">
+                    <div className="space-y-3">
+                      <div className="py-6 px-4 space-y-2 bg-white bg-opacity-15 rounded-lg text-center">
+                        <h3 className="text-2xl md:text-4xl font-bold">
+                          14,766
+                        </h3>
+                        <h4>Naira</h4>
+                      </div>
+                      <div className="text-center text-lg">
+                        <p className="capitalize text-white/60 text-sm">
+                          This week
+                        </p>
+                        <span className="text-[#F90808] font-semibold flex items-center justify-center">
+                          3.65%
+                          <FiArrowDownRight className="size-6" />
+                        </span>
+                      </div>
                     </div>
-                    <div className="text-center text-lg">
-                      <p className="capitalize text-sm">This Month</p>
-                      <span className="text-[#F90808] font-semibold flex items-center justify-center">
-                        3.65%
-                        <FiArrowDownRight className="size-6" />
-                      </span>
+                    <div className="space-y-3">
+                      <div className="py-6 px-4 space-y-2 bg-white bg-opacity-15 rounded-lg text-center">
+                        <h3 className="text-2xl md:text-4xl font-bold">
+                          15,766
+                        </h3>
+                        <h4>Naira</h4>
+                      </div>
+                      <div className="text-center text-lg">
+                        <p className="capitalize text-white/60 text-sm">
+                          last week
+                        </p>
+                        <span className="text-[#26A17B] font-semibold flex items-center justify-center">
+                          5.85%
+                          <FiArrowUpRight className="size-6" />
+                        </span>
+                      </div>
                     </div>
                   </div>
-                  <div className="space-y-3">
-                    <div className="py-6 px-4 space-y-2 bg-[#EFEFEF] rounded-lg text-center">
-                      <h3 className="text-2xl md:text-4xl font-bold">45,766</h3>
-                      <h4>Naira</h4>
+                </div>
+
+                <div className="p-6 rounded-3xl flex flex-col gap-6 shadow-xl text-[#444444]">
+                  <h3 className="text-[#444444] text-xl sm:text-2xl font-medium">
+                    Total Revenue Generated
+                  </h3>
+                  <div className="grid sm:grid-cols-2 gap-5 sm:gap-10">
+                    <div className="space-y-3">
+                      <div className="py-6 px-4 space-y-2 bg-[#EFEFEF] rounded-lg text-center">
+                        <h3 className="text-2xl md:text-4xl font-bold">
+                          14,766
+                        </h3>
+                        <h4>Naira</h4>
+                      </div>
+                      <div className="text-center text-lg">
+                        <p className="capitalize text-sm">This Month</p>
+                        <span className="text-[#F90808] font-semibold flex items-center justify-center">
+                          3.65%
+                          <FiArrowDownRight className="size-6" />
+                        </span>
+                      </div>
                     </div>
-                    <div className="text-center text-lg">
-                      <p className="capitalize text-sm">last Month</p>
-                      <span className="text-[#26A17B] font-semibold flex items-center justify-center">
-                        26.34%
-                        <FiArrowUpRight className="size-6" />
-                      </span>
+                    <div className="space-y-3">
+                      <div className="py-6 px-4 space-y-2 bg-[#EFEFEF] rounded-lg text-center">
+                        <h3 className="text-2xl md:text-4xl font-bold">
+                          45,766
+                        </h3>
+                        <h4>Naira</h4>
+                      </div>
+                      <div className="text-center text-lg">
+                        <p className="capitalize text-sm">last Month</p>
+                        <span className="text-[#26A17B] font-semibold flex items-center justify-center">
+                          26.34%
+                          <FiArrowUpRight className="size-6" />
+                        </span>
+                      </div>
                     </div>
                   </div>
                 </div>
